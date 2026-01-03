43a349752f9242789cb800616ce2bc643b39a30b
artiverma-00 <vermaarti930@gmail.com>
2026-01-03 19:31:11 +0530

    protected and localstrog updated

---
+++ src/utils/localStorage.jsx
@@ -0,0 +1,7 @@
+export const getRestaurants=()=>JSON.parse(localStorage.getItem("evalData"))||[];
+export const saveRestaurants = (data)=>localStorage.setItem("evalData", JSON.stringify(data));
+
+
+
+
+
